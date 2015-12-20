38c69dca2aaabc24de24e50d1a987bec21d78527
Ruben Gees <geesruben@yahoo.de>
2015-12-20 17:22:13 +0100

    Translations

--- app/src/main/java/com/proxerme/app/service/NotificationService.java
+++ app/src/main/java/com/proxerme/app/service/NotificationService.java
@@ -102,6 +102,7 @@ public class NotificationService extends IntentService {
             }
 
             StorageManager.incrementMessagesInterval();
+            NotificationRetrievalManager.retrieveMessagesLater(this);
         } else {
             PreferenceManager.setMessagesNotificationsEnabled(this, false);
             NotificationRetrievalManager.cancelMessagesRetrieval(this);
