552fd8b39aafc6ede31a2026654d7c702afecad7
Ruben Gees <geesruben@yahoo.de>
2016-11-14 23:11:56 +0100

    Switch to Exomedia (Exoplayer)
    - This provides a way better player framework with support for many formats and higher quality

--- app/src/main/kotlin/com/proxerme/app/activity/StreamActivity.kt
+++ app/src/main/kotlin/com/proxerme/app/activity/StreamActivity.kt
@@ -1,78 +1,102 @@
 package com.proxerme.app.activity
 
-import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
-import android.telephony.PhoneStateListener
-import android.telephony.TelephonyManager
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
+import android.view.View.*
+import android.view.WindowManager
 import butterknife.bindView
-import com.afollestad.easyvideoplayer.EasyVideoCallback
-import com.afollestad.easyvideoplayer.EasyVideoPlayer
+import com.devbrackets.android.exomedia.listener.VideoControlsVisibilityListener
+import com.devbrackets.android.exomedia.ui.widget.EMVideoView
 import com.proxerme.app.R
-import com.proxerme.app.util.telephonyManager
-
 
 class StreamActivity : AppCompatActivity() {
 
-    private val player: EasyVideoPlayer by bindView(R.id.player)
+    private val toolbar: Toolbar by bindView(R.id.toolbar)
+    private val player: EMVideoView by bindView(R.id.player)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stream)
 
-        player.setSource(intent.data)
-        player.setAutoPlay(true)
-        player.setAutoFullscreen(true)
-        player.setCallback(object : EasyVideoCallback {
-            override fun onPrepared(player: EasyVideoPlayer?) {
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        title = null
+
+        player.setBackgroundColor(ContextCompat.getColor(this, R.color.md_black_1000))
+        player.setVideoURI(intent.data)
+        player.videoControls?.setVisibilityListener(object : VideoControlsVisibilityListener {
+            override fun onControlsShown() {
+                toggleFullscreen(false)
             }
 
-            override fun onStarted(player: EasyVideoPlayer?) {
-            }
-
-            override fun onCompletion(player: EasyVideoPlayer?) {
-            }
-
-            override fun onRetry(player: EasyVideoPlayer?, source: Uri?) {
-            }
-
-            override fun onSubmit(player: EasyVideoPlayer?, source: Uri?) {
-            }
-
-            override fun onBuffering(percent: Int) {
-            }
-
-            override fun onPreparing(player: EasyVideoPlayer?) {
-            }
-
-            override fun onError(player: EasyVideoPlayer?, e: Exception?) {
-            }
-
-            override fun onPaused(player: EasyVideoPlayer?) {
+            override fun onControlsHidden() {
+                toggleFullscreen(true)
             }
         })
+        player.setOnPreparedListener {
+            player.start()
+        }
 
-        telephonyManager.listen(object : PhoneStateListener() {
-            override fun onCallStateChanged(state: Int, incomingNumber: String) {
-                when (state) {
-                    TelephonyManager.CALL_STATE_OFFHOOK, TelephonyManager.CALL_STATE_RINGING ->
-                        player.pause()
-                    TelephonyManager.CALL_STATE_IDLE ->
-                        player.start()
-                }
-            }
-        }, PhoneStateListener.LISTEN_CALL_STATE)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        }
+
+        toggleFullscreen(true)
     }
 
-    public override fun onPause() {
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    public override fun onStop() {
         player.pause()
 
         super.onPause()
     }
 
     override fun onDestroy() {
-        player.release()
+        toggleFullscreen(false)
 
         super.onDestroy()
+    }
+
+    private fun toggleFullscreen(fullscreen: Boolean) {
+        window.decorView.systemUiVisibility = if (fullscreen) getFullscreenUiFlags() else
+            SYSTEM_UI_FLAG_VISIBLE
+        window.decorView.setOnSystemUiVisibilityChangeListener {
+            if (it and SYSTEM_UI_FLAG_FULLSCREEN === 0) {
+                player.showControls()
+            }
+        }
+
+        toolbar.visibility = when (fullscreen) {
+            true -> GONE
+            false -> VISIBLE
+        }
+    }
+
+    private fun getFullscreenUiFlags(): Int {
+        var flags = SYSTEM_UI_FLAG_LOW_PROFILE or SYSTEM_UI_FLAG_HIDE_NAVIGATION
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            flags = flags or (SYSTEM_UI_FLAG_LAYOUT_STABLE or SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or SYSTEM_UI_FLAG_FULLSCREEN or
+                    SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+        }
+
+        return flags
     }
 }
