16641044c7d010b4d89cd583c46b2342a07b45a9
Ruben Gees <geesruben@yahoo.de>
2016-11-25 18:27:15 +0100

    Make the TouchImageView inherit from AppCompatImageView

--- app/src/main/java/view/TouchImageView.java
+++ app/src/main/java/view/TouchImageView.java
@@ -26,6 +26,7 @@ import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.GestureDetector;
@@ -33,10 +34,9 @@ import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
-import android.widget.ImageView;
 import android.widget.OverScroller;
 
-public class TouchImageView extends ImageView {
+public class TouchImageView extends AppCompatImageView {
 
     private static final String DEBUG = "DEBUG";
 
