f36ab3609b3ac4ad5ff749d0b0f598b6eabda251
Ruben Gees <geesruben@yahoo.de>
2017-01-22 17:34:57 +0100

    Finish the projects list for industries

--- app/src/main/kotlin/com/proxerme/app/adapter/info/ProjectAdapter.kt
+++ app/src/main/kotlin/com/proxerme/app/adapter/info/ProjectAdapter.kt
@@ -14,6 +14,8 @@ import com.proxerme.app.adapter.framework.PagingAdapter
 import com.proxerme.app.util.bindView
 import com.proxerme.library.connection.list.entity.ProjectListEntry
 import com.proxerme.library.info.ProxerUrlHolder
+import com.proxerme.library.parameters.ProjectTypeParameter
+import com.proxerme.library.parameters.StateParameter
 
 /**
  * TODO: Describe class
@@ -44,8 +46,9 @@ class ProjectAdapter : PagingAdapter<ProjectListEntry>() {
         private val title: TextView by bindView(R.id.title)
         private val medium: TextView by bindView(R.id.medium)
         private val image: ImageView by bindView(R.id.image)
+        private val ratingContainer: ViewGroup by bindView(R.id.ratingContainer)
         private val rating: RatingBar by bindView(R.id.rating)
-        private val episodes: TextView by bindView(R.id.episodes)
+        private val status: TextView by bindView(R.id.status)
 
         init {
             itemView.setOnClickListener {
@@ -58,19 +61,39 @@ class ProjectAdapter : PagingAdapter<ProjectListEntry>() {
         override fun bind(item: ProjectListEntry) {
             title.text = item.name
             medium.text = item.medium
-//            episodes.text = item.state
+            status.text = status.context.getString(generateStatusText(item.type, item.state))
 
             if (item.rating > 0) {
-                rating.visibility = View.VISIBLE
+                ratingContainer.visibility = View.VISIBLE
                 rating.rating = item.rating / 2.0f
             } else {
-                rating.visibility = View.GONE
+                ratingContainer.visibility = View.GONE
             }
 
             Glide.with(image.context)
                     .load(ProxerUrlHolder.getCoverImageUrl(item.id).toString())
                     .diskCacheStrategy(DiskCacheStrategy.SOURCE)
                     .into(image)
+        }
+
+        private fun generateStatusText(type: String, state: Int): Int {
+            return when (type.toIntOrNull()) {
+                ProjectTypeParameter.UNDEFINED -> R.string.project_type_unkown
+                ProjectTypeParameter.FINISHED -> R.string.project_type_finished
+                ProjectTypeParameter.IN_WORK -> R.string.project_type_in_work
+                ProjectTypeParameter.PLANNED -> R.string.project_type_planned
+                ProjectTypeParameter.CANCELLED -> R.string.project_type_cancelled
+                ProjectTypeParameter.LICENCED -> R.string.project_type_licenced
+                null -> when (state) {
+                    StateParameter.PRE_AIRING -> R.string.media_state_pre_airing
+                    StateParameter.AIRING -> R.string.media_state_airing
+                    StateParameter.CANCELLED -> R.string.media_state_cancelled
+                    StateParameter.CANCELLED_SUB -> R.string.media_state_cancelled_sub
+                    StateParameter.FINISHED -> R.string.media_state_finished
+                    else -> throw IllegalArgumentException("Unknown state: $state")
+                }
+                else -> throw IllegalArgumentException("Unknown project type: ${type}")
+            }
         }
     }
 
