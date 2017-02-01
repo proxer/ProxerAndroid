0bf71743ae07293176f145804d9a041003549fab
Ruben Gees <rubengees7@googlemail.com>
2017-02-01 22:38:57 +0100

    Refactor the StreamActivity to show messages in case of an error

--- app/src/main/kotlin/com/proxerme/app/activity/StreamActivity.kt
+++ app/src/main/kotlin/com/proxerme/app/activity/StreamActivity.kt
@@ -2,10 +2,12 @@ package com.proxerme.app.activity
 
 import android.os.Build
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.Toolbar
 import android.view.MenuItem
 import android.view.View.*
+import android.view.ViewGroup
 import android.view.WindowManager
 import com.devbrackets.android.exomedia.listener.VideoControlsVisibilityListener
 import com.devbrackets.android.exomedia.ui.widget.EMVideoView
@@ -15,6 +17,10 @@ import org.jetbrains.anko.longToast
 
 class StreamActivity : MainActivity() {
 
+    private val uri
+        get() = intent.data
+
+    private val root: ViewGroup by bindView(R.id.root)
     private val toolbar: Toolbar by bindView(R.id.toolbar)
     private val player: EMVideoView by bindView(R.id.player)
 
@@ -24,30 +30,8 @@ class StreamActivity : MainActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
             setContentView(R.layout.activity_stream)
 
-            setSupportActionBar(toolbar)
-
-            supportActionBar?.setDisplayHomeAsUpEnabled(true)
-            title = null
-
-            player.setBackgroundColor(ContextCompat.getColor(this, R.color.md_black_1000))
-            player.setVideoURI(intent.data)
-            player.videoControls?.setVisibilityListener(object : VideoControlsVisibilityListener {
-                override fun onControlsShown() {
-                    toggleFullscreen(false)
-                }
-
-                override fun onControlsHidden() {
-                    toggleFullscreen(true)
-                }
-            })
-
-            player.setOnPreparedListener {
-                player.start()
-            }
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-            }
+            setupToolbar()
+            setupPlayer()
 
             toggleFullscreen(true)
         } else {
@@ -75,12 +59,44 @@ class StreamActivity : MainActivity() {
         super.onPause()
     }
 
-    override fun onDestroy() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-            toggleFullscreen(false)
+    private fun setupPlayer() {
+        player.setBackgroundColor(ContextCompat.getColor(this, R.color.md_black_1000))
+        player.setVideoURI(uri)
+        player.setOnErrorListener {
+            Snackbar.make(root, R.string.error_unknown, Snackbar.LENGTH_INDEFINITE)
+                    .setAction(R.string.error_action_retry, {
+                        player.reset()
+                        player.setVideoURI(uri)
+                        player.start()
+                    }).show()
+
+            false
         }
 
-        super.onDestroy()
+        player.videoControls?.setVisibilityListener(object : VideoControlsVisibilityListener {
+            override fun onControlsShown() {
+                toggleFullscreen(false)
+            }
+
+            override fun onControlsHidden() {
+                toggleFullscreen(true)
+            }
+        })
+
+        player.setOnPreparedListener {
+            player.start()
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        }
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        title = null
     }
 
     private fun toggleFullscreen(fullscreen: Boolean) {
