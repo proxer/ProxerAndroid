5e3260afdc6719a4bc267b07b79f5446e35e7213
Ruben Gees <rubengees7@googlemail.com>
2017-02-02 01:29:27 +0100

    Add a prototype for Myvi
    Not in use yet as the StreamActivity can not handle the result

---
+++ app/src/main/kotlin/com/proxerme/app/stream/resolver/MyviResolver.kt
@@ -0,0 +1,40 @@
+package com.proxerme.app.stream.resolver
+
+import com.proxerme.app.application.MainApplication
+import com.proxerme.app.task.StreamResolutionTask.StreamResolutionResult
+import com.proxerme.app.util.androidUri
+import okhttp3.HttpUrl
+import okhttp3.Request
+
+
+/**
+ * Resolver for myvi.ru. It is currently not added, as the StreamActivity can not handle the result
+ * properly.
+ *
+ * @author Ruben Gees
+ */
+class MyviResolver : StreamResolver() {
+
+    override val name = "myvi.ru"
+
+    override fun resolve(url: HttpUrl): StreamResolutionResult {
+        val response = MainApplication.proxerConnection.httpClient.newCall(Request.Builder()
+                .get()
+                .url("http://myvi.ru/player/api/Video/Get/${url.pathSegments().last()}?sig")
+                .build()).execute()
+
+        val resultUrl = MainApplication.proxerConnection.moshi.adapter(SprutoResult::class.java)
+                .fromJson(validateAndGetResult(response)).url
+
+        return StreamResolutionResult(resultUrl.androidUri(), "video/mp4")
+    }
+
+    private class SprutoResult(private val sprutoData: SprutoData) {
+        val url
+            get() = HttpUrl.parse(sprutoData.playlist.first().video.first().url)
+    }
+
+    private class SprutoData(val playlist: Array<SprutoPlaylistItem>)
+    private class SprutoPlaylistItem(val video: Array<SprutoVideo>)
+    private class SprutoVideo(val url: String)
+}
