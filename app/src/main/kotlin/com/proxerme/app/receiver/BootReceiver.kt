9e7dfd2d2a356371bcca21a87e88defdcdbe72a4
Ruben Gees <rubengees7@googlemail.com>
2017-02-14 03:42:42 +0100

    Cleanup

--- app/src/main/kotlin/com/proxerme/app/receiver/BootReceiver.kt
+++ app/src/main/kotlin/com/proxerme/app/receiver/BootReceiver.kt
@@ -8,13 +8,13 @@ import com.proxerme.app.service.ChatService
 
 /**
  * Receiver for a boot. It starts and schedules all background services and tasks.
-
+ *
  * @author Ruben Gees
  */
 class BootReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        if (intent.action == "android.intent.action.BOOT_COMPLETED") {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
             ServiceHelper.retrieveNewsLater(context)
             ChatService.reschedule(context)
         }
