6a428a47ed10af3743eb06b6d3cd01aea60caa02
Ruben Gees <rubengees7@googlemail.com>
2017-06-11 02:13:54 +0200

    Improve readability of image tab Activities

--- app/src/main/kotlin/me/proxer/app/activity/base/ImageTabsActivity.kt
+++ app/src/main/kotlin/me/proxer/app/activity/base/ImageTabsActivity.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 import android.support.design.widget.AppBarLayout
 import android.support.design.widget.CollapsingToolbarLayout
 import android.support.design.widget.TabLayout
-import android.support.v4.content.ContextCompat
 import android.support.v4.view.PagerAdapter
 import android.support.v4.view.ViewCompat
 import android.support.v4.view.ViewPager
@@ -15,7 +14,6 @@ import android.support.v7.widget.Toolbar
 import android.transition.Transition
 import android.view.MenuItem
 import android.widget.ImageView
-import android.widget.TextView
 import com.bumptech.glide.request.target.ImageViewTarget
 import com.h6ah4i.android.tablayouthelper.TabLayoutHelper
 import me.proxer.app.R
@@ -25,7 +23,6 @@ import me.proxer.app.util.ActivityUtils
 import me.proxer.app.util.extension.bindView
 import me.proxer.app.util.listener.TransitionListenerWrapper
 import okhttp3.HttpUrl
-import org.jetbrains.anko.applyRecursively
 
 /**
  * @author Ruben Gees
@@ -145,17 +142,6 @@ abstract class ImageTabsActivity : MainActivity() {
         appbar.addOnOffsetChangedListener { _, verticalOffset ->
             isHeaderImageVisible = collapsingToolbar.height + verticalOffset >
                     collapsingToolbar.scrimVisibleHeightTrigger
-
-            listOf(tabs, toolbar).forEach {
-                it.applyRecursively {
-                    if (it is TextView) {
-                        when (isHeaderImageVisible) {
-                            true -> it.setShadowLayer(4f, 0f, 0f, ContextCompat.getColor(this, android.R.color.black))
-                            false -> it.setShadowLayer(0f, 0f, 0f, 0)
-                        }
-                    }
-                }
-            }
         }
     }
 
