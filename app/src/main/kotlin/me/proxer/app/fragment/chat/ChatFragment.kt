c4a97ea9de8835c09b44856ce6be7b4680973212
Ruben Gees <rubengees7@googlemail.com>
2017-06-12 21:13:45 +0200

    Also cancel notifications in the chat screen

--- app/src/main/kotlin/me/proxer/app/fragment/chat/ChatFragment.kt
+++ app/src/main/kotlin/me/proxer/app/fragment/chat/ChatFragment.kt
@@ -16,7 +16,6 @@ import com.mikepenz.iconics.utils.IconicsMenuInflatorUtil
 import com.rubengees.ktask.util.TaskBuilder
 import com.vanniktech.emoji.EmojiEditText
 import com.vanniktech.emoji.EmojiPopup
-import me.proxer.app.R
 import me.proxer.app.activity.ChatActivity
 import me.proxer.app.activity.ProfileActivity
 import me.proxer.app.adapter.chat.ChatAdapter
@@ -24,6 +23,7 @@ import me.proxer.app.application.MainApplication.Companion.chatDb
 import me.proxer.app.entity.chat.LocalConference
 import me.proxer.app.entity.chat.LocalMessage
 import me.proxer.app.fragment.base.PagedLoadingFragment
+import me.proxer.app.helper.NotificationHelper
 import me.proxer.app.helper.StorageHelper
 import me.proxer.app.job.ChatJob
 import me.proxer.app.task.chat.ChatRefreshTask
@@ -189,6 +189,8 @@ class ChatFragment : PagedLoadingFragment<Int, LocalMessage>() {
         super.onResume()
 
         isActive = true
+
+        NotificationHelper.cancelChatNotification(context)
 
         if (!isFirstStart && !ChatJob.isRunning()) {
             ChatJob.scheduleSynchronization()
