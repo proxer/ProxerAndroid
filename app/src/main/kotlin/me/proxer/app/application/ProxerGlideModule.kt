3cc3778a4c9eb887998803be7a396c33b5de3e40
Ruben Gees <rubengees7@googlemail.com>
2017-07-13 22:57:04 +0200

    Adjust for the latest Glide snapshot

--- app/src/main/kotlin/me/proxer/app/application/ProxerGlideModule.kt
+++ app/src/main/kotlin/me/proxer/app/application/ProxerGlideModule.kt
@@ -1,12 +1,12 @@
 package me.proxer.app.application
 
 import android.content.Context
+import com.bumptech.glide.Glide
 import com.bumptech.glide.Registry
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
 import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.module.AppGlideModule
-import me.proxer.app.application.MainApplication.Companion.client
 import java.io.InputStream
 
 /**
@@ -15,8 +15,8 @@ import java.io.InputStream
 @GlideModule
 class ProxerGlideModule : AppGlideModule() {
 
-    override fun registerComponents(context: Context, registry: Registry) {
-        registry.replace(GlideUrl::class.java, InputStream::class.java, OkHttpUrlLoader.Factory(client))
+    override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
+        registry.replace(GlideUrl::class.java, InputStream::class.java, OkHttpUrlLoader.Factory(MainApplication.client))
     }
 
     override fun isManifestParsingEnabled() = false
