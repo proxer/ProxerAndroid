ff0cb2e70d0261f1e6495765075d5c0a19cbad29
Ruben Gees <rubengees7@googlemail.com>
2017-08-10 16:45:01 +0200

    Add forgotten stream resolvers and update gradle plugin

--- app/src/main/kotlin/me/proxer/app/anime/resolver/StreamResolverFactory.kt
+++ app/src/main/kotlin/me/proxer/app/anime/resolver/StreamResolverFactory.kt
@@ -8,8 +8,9 @@ object StreamResolverFactory {
     private val resolvers = arrayOf(AkibaPassStreamResolver(), AmazonPrimeVideoStreamResolver(),
             AnimeOnDemandStreamResolver(), AuravidStreamResolver(), ClipfishStreamResolver(), CodeStreamResolver(),
             CrunchyrollStreamResolver(), DailymotionStreamResolver(), Mp4UploadStreamResolver(), MyviStreamResolver(),
-            OpenloadStreamResolver(), ProxerStreamResolver(), StreamcloudStreamResolver(), VideoweedStreamResolver(),
-            ViewsterStreamResolver(), YourUploadStreamResolver())
+            OpenloadStreamResolver(), ProsiebenMAXXStreamResolver(), ProxerStreamResolver(),
+            StreamcloudStreamResolver(), VideoweedStreamResolver(), ViewsterStreamResolver(),
+            YourUploadStreamResolver(), YouTubeStreamResolver())
 
     fun resolverFor(name: String) = resolvers.find { it.supports(name) }
 }
