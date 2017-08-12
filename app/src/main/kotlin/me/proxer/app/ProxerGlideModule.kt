117473d4e8d26e8014ef527fd35beb7edc3c0538
Ruben Gees <rubengees7@googlemail.com>
2017-08-12 18:53:14 +0200

    Exclude the OkHttpLibraryGlideModule as we supply our own

--- app/src/main/kotlin/me/proxer/app/ProxerGlideModule.kt
+++ app/src/main/kotlin/me/proxer/app/ProxerGlideModule.kt
@@ -3,7 +3,9 @@ package me.proxer.app
 import android.content.Context
 import com.bumptech.glide.Glide
 import com.bumptech.glide.Registry
+import com.bumptech.glide.annotation.Excludes
 import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.integration.okhttp3.OkHttpLibraryGlideModule
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
 import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.module.AppGlideModule
@@ -14,6 +16,7 @@ import java.io.InputStream
  * @author Ruben Gees
  */
 @GlideModule
+@Excludes(OkHttpLibraryGlideModule::class)
 class ProxerGlideModule : AppGlideModule() {
 
     override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
