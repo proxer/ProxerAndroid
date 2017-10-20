d7e028533069a7796e627afe32cb10cde77a11bd
Ruben Gees <rubengees7@googlemail.com>
2017-10-20 23:37:45 +0200

    Fix Chrunchyroll compability

--- src/main/kotlin/me/proxer/app/anime/resolver/CrunchyrollStreamResolver.kt
+++ src/main/kotlin/me/proxer/app/anime/resolver/CrunchyrollStreamResolver.kt
@@ -3,17 +3,12 @@ package me.proxer.app.anime.resolver
 import android.content.Intent
 import android.net.Uri
 import io.reactivex.Single
-import me.proxer.app.MainApplication.Companion.USER_AGENT
 import me.proxer.app.MainApplication.Companion.api
-import me.proxer.app.MainApplication.Companion.client
 import me.proxer.app.MainApplication.Companion.globalContext
 import me.proxer.app.exception.AppRequiredException
 import me.proxer.app.exception.StreamResolutionException
 import me.proxer.app.util.Utils
 import me.proxer.app.util.extension.buildSingle
-import me.proxer.app.util.extension.toBodySingle
-import okhttp3.Request
-import java.util.regex.Pattern.quote
 
 /**
  * @author Ruben Gees
@@ -22,7 +17,7 @@ class CrunchyrollStreamResolver : StreamResolver() {
 
     private companion object {
         private const val CRUNCHYROLL_PACKAGE = "com.crunchyroll.crunchyroid"
-        private val regex = Regex("mediaMetadata = ${quote("{")}\"id\":(\\d*),")
+        private val regex = Regex("media_id=(\\d*)?")
     }
 
     override val name = "Crunchyroll"
@@ -35,16 +30,8 @@ class CrunchyrollStreamResolver : StreamResolver() {
                 }
             }
             .flatMap { api.anime().link(id).buildSingle() }
-            .flatMap { url ->
-                client.newCall(Request.Builder()
-                        .get()
-                        .url(Utils.parseAndFixUrl(url))
-                        .header("User-Agent", USER_AGENT)
-                        .build())
-                        .toBodySingle()
-            }
-            .map {
-                val regexResult = regex.find(it) ?: throw StreamResolutionException()
+            .map { url ->
+                val regexResult = regex.find(url) ?: throw StreamResolutionException()
                 val mediaId = regexResult.groupValues[1]
 
                 if (mediaId.isBlank()) {
