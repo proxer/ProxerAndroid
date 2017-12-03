e14e2570bddec8157a05d53618feb96e0ebb4bc0
Ruben Gees <rubengees7@googlemail.com>
2017-12-03 23:12:44 +0100

    Update Glide which fixes the in-process compilation for CI

--- src/main/kotlin/me/proxer/app/ProxerGlideModule.kt
+++ src/main/kotlin/me/proxer/app/ProxerGlideModule.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import com.bumptech.glide.Glide
 import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.Registry
+import com.bumptech.glide.annotation.Excludes
 import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.integration.okhttp3.OkHttpLibraryGlideModule
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
 import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.module.AppGlideModule
@@ -16,6 +18,7 @@ import java.io.InputStream
  * @author Ruben Gees
  */
 @GlideModule
+@Excludes(OkHttpLibraryGlideModule::class)
 class ProxerGlideModule : AppGlideModule() {
 
     override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
