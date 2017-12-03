395f7742b33b59dcdc920e904df9e82d8aae127b
Ruben Gees <rubengees7@googlemail.com>
2017-12-03 20:03:11 +0100

    Add an informative message to the login dialog on registration

--- src/main/kotlin/me/proxer/app/auth/LoginDialog.kt
+++ src/main/kotlin/me/proxer/app/auth/LoginDialog.kt
@@ -5,15 +5,21 @@ import android.arch.lifecycle.Observer
 import android.os.Bundle
 import android.support.design.widget.TextInputEditText
 import android.support.design.widget.TextInputLayout
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
 import android.view.inputmethod.EditorInfo
 import android.widget.ProgressBar
+import android.widget.TextView
 import com.afollestad.materialdialogs.MaterialDialog
 import com.jakewharton.rxbinding2.widget.editorActionEvents
 import com.jakewharton.rxbinding2.widget.textChanges
+import com.klinker.android.link_builder.Link
+import com.klinker.android.link_builder.LinkBuilder
+import com.mikepenz.community_material_typeface_library.CommunityMaterial
+import com.mikepenz.iconics.IconicsDrawable
 import io.reactivex.functions.Predicate
 import kotterknife.bindView
 import me.proxer.app.MainApplication.Companion.bus
@@ -21,8 +27,11 @@ import me.proxer.app.R
 import me.proxer.app.base.BaseDialog
 import me.proxer.app.util.data.StorageHelper
 import me.proxer.app.util.extension.autoDispose
+import me.proxer.app.util.extension.dip
 import me.proxer.app.util.extension.unsafeLazy
+import me.proxer.library.util.ProxerUrls
 import org.jetbrains.anko.longToast
+import java.util.regex.Pattern
 
 /**
  * @author Ruben Gees
@@ -30,6 +39,8 @@ import org.jetbrains.anko.longToast
 class LoginDialog : BaseDialog() {
 
     companion object {
+        private val WEBSITE_PATTERN = Pattern.compile("Proxer \\b(.+?)\\b", Pattern.DOTALL)
+
         fun show(activity: AppCompatActivity) = LoginDialog().show(activity.supportFragmentManager, "login_dialog")
     }
 
@@ -42,6 +53,13 @@ class LoginDialog : BaseDialog() {
     private val passwordContainer: TextInputLayout by bindView(R.id.passwordContainer)
     private val inputContainer: ViewGroup by bindView(R.id.inputContainer)
     private val progress: ProgressBar by bindView(R.id.progress)
+    private val registrationInfo: TextView by bindView(R.id.registrationInfo)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setLikelyUrl(ProxerUrls.webBase())
+    }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog = MaterialDialog.Builder(safeContext)
             .autoDismiss(false)
@@ -82,6 +100,18 @@ class LoginDialog : BaseDialog() {
         }
 
         secret.transformationMethod = null
+
+        registrationInfo.text = safeContext.getString(R.string.dialog_login_registration)
+        registrationInfo.compoundDrawablePadding = dip(12)
+
+        registrationInfo.setCompoundDrawables(generateInfoDrawable(), null, null, null)
+
+        LinkBuilder.on(registrationInfo)
+                .addLink(Link(WEBSITE_PATTERN)
+                        .setTextColor(ContextCompat.getColor(safeContext, R.color.link))
+                        .setUnderlined(false)
+                        .setOnClickListener { showPage(ProxerUrls.webBase()) })
+                .build()
     }
 
     private fun setupViewModels() {
@@ -143,4 +173,9 @@ class LoginDialog : BaseDialog() {
         container.isErrorEnabled = errorText != null
         container.error = errorText
     }
+
+    private fun generateInfoDrawable() = IconicsDrawable(safeContext)
+            .icon(CommunityMaterial.Icon.cmd_information_outline)
+            .colorRes(R.color.icon)
+            .sizeDp(20)
 }
--- src/main/kotlin/me/proxer/app/base/BaseDialog.kt
+++ src/main/kotlin/me/proxer/app/base/BaseDialog.kt
@@ -1,8 +1,15 @@
 package me.proxer.app.base
 
+import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import kotterknife.KotterKnife
 import me.proxer.app.MainApplication.Companion.refWatcher
+import me.proxer.app.util.extension.androidUri
+import me.proxer.app.util.extension.openHttpPage
+import me.zhanghai.android.customtabshelper.CustomTabsHelperFragment
+import okhttp3.HttpUrl
+import org.jetbrains.anko.bundleOf
+import kotlin.properties.Delegates
 
 /**
  * @author Ruben Gees
@@ -15,6 +22,14 @@ abstract class BaseDialog : DialogFragment() {
     val safeArguments get() = arguments ?: throw IllegalStateException("arguments are null")
     val safeTargetFragment get() = targetFragment ?: throw IllegalStateException("targetFragment is null")
 
+    private var customTabsHelper by Delegates.notNull<CustomTabsHelperFragment>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        customTabsHelper = CustomTabsHelperFragment.attachTo(this)
+    }
+
     override fun onDestroyView() {
         KotterKnife.reset(this)
 
@@ -26,4 +41,7 @@ abstract class BaseDialog : DialogFragment() {
 
         refWatcher.watch(this)
     }
+
+    fun setLikelyUrl(url: HttpUrl) = customTabsHelper.mayLaunchUrl(url.androidUri(), bundleOf(), emptyList())
+    fun showPage(url: HttpUrl) = customTabsHelper.openHttpPage(safeActivity, url)
 }
