376d57b56324edfcd751b0b125a4b40ec0d8b421
Ruben Gees <rubengees7@googlemail.com>
2018-01-01 22:20:31 +0100

    Make the table BBCode parser return more performant layouts and beautify

--- src/main/kotlin/me/proxer/app/ui/view/bbcode/table/TableCellTree.kt
+++ src/main/kotlin/me/proxer/app/ui/view/bbcode/table/TableCellTree.kt
@@ -2,6 +2,7 @@ package me.proxer.app.ui.view.bbcode.table
 
 import android.content.Context
 import android.view.View
+import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.TableRow.LayoutParams
 import android.widget.TableRow.LayoutParams.WRAP_CONTENT
@@ -18,11 +19,23 @@ class TableCellTree(parent: BBTree?, children: MutableList<BBTree> = mutableList
     override fun makeViews(context: Context): List<View> {
         val children = super.makeViews(context)
 
-        return listOf(LinearLayout(context).apply {
-            layoutParams = LayoutParams(0, WRAP_CONTENT, 1f)
-            orientation = VERTICAL
+        return when (children.size) {
+            0, 1 -> children.map {
+                when (it.layoutParams?.width ?: -1 > 0 || it.layoutParams?.height ?: -1 > 0) {
+                    true -> FrameLayout(context).apply {
+                        layoutParams = LayoutParams(0, WRAP_CONTENT, 1f)
 
-            children.forEach { addView(it) }
-        })
+                        addView(it)
+                    }
+                    false -> it.apply { layoutParams = LayoutParams(0, WRAP_CONTENT, 1f) }
+                }
+            }
+            else -> listOf(LinearLayout(context).apply {
+                layoutParams = LayoutParams(0, WRAP_CONTENT, 1f)
+                orientation = VERTICAL
+
+                children.forEach { addView(it) }
+            })
+        }
     }
 }
--- src/main/kotlin/me/proxer/app/ui/view/bbcode/table/TableTree.kt
+++ src/main/kotlin/me/proxer/app/ui/view/bbcode/table/TableTree.kt
@@ -2,11 +2,13 @@ package me.proxer.app.ui.view.bbcode.table
 
 import android.content.Context
 import android.view.View
+import android.view.ViewGroup.MarginLayoutParams
 import android.widget.LinearLayout.LayoutParams
 import android.widget.LinearLayout.LayoutParams.MATCH_PARENT
 import android.widget.LinearLayout.LayoutParams.WRAP_CONTENT
 import android.widget.TableLayout
 import me.proxer.app.ui.view.bbcode.BBTree
+import org.jetbrains.anko.dip
 
 /**
  * @author Ruben Gees
@@ -17,6 +19,12 @@ class TableTree(parent: BBTree?, children: MutableList<BBTree> = mutableListOf()
 
     override fun makeViews(context: Context): List<View> {
         val children = children.filterIsInstance(TableRowTree::class.java).flatMap { it.makeViews(context) }
+
+        for (child in children.dropLast(1)) {
+            (child.layoutParams as? MarginLayoutParams)?.apply {
+                setMargins(0, 0, 0, context.dip(4))
+            }
+        }
 
         return listOf(TableLayout(context).apply {
             layoutParams = LayoutParams(MATCH_PARENT, WRAP_CONTENT)
