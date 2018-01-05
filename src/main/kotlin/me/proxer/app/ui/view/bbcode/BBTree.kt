56daff15d5288d5cad464f48ef6d1ca4cae6dd8f
Ruben Gees <rubengees7@googlemail.com>
2018-01-05 15:26:31 +0100

    Implement proper equals and hashcode methods for the BBTree

--- src/main/kotlin/me/proxer/app/ui/view/bbcode/BBTree.kt
+++ src/main/kotlin/me/proxer/app/ui/view/bbcode/BBTree.kt
@@ -29,4 +29,25 @@ open class BBTree(
 
     fun endsWith(code: String) = prototype.endRegex.matches(code)
     fun makeViews(context: Context) = prototype.makeViews(context, children, args.plus(GLIDE_ARGUMENT to glide))
+
+    @Suppress("EqualsAlwaysReturnsTrueOrFalse") // False positive
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as BBTree
+
+        if (prototype != other.prototype) return false
+        if (children != other.children) return false
+        if (args != other.args) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = prototype.hashCode()
+        result = 31 * result + children.hashCode()
+        result = 31 * result + args.hashCode()
+        return result
+    }
 }
