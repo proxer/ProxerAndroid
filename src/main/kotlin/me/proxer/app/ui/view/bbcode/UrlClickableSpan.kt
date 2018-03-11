27c508ffd425fa1e396ab04f3792330f22e22aef
Ruben Gees <rubengees7@googlemail.com>
2018-03-11 18:48:14 +0100

    Fix clicks in BBCodeViews on Android < 5

--- src/main/kotlin/me/proxer/app/ui/view/bbcode/UrlClickableSpan.kt
+++ src/main/kotlin/me/proxer/app/ui/view/bbcode/UrlClickableSpan.kt
@@ -1,5 +1,7 @@
 package me.proxer.app.ui.view.bbcode
 
+import android.content.Context
+import android.content.ContextWrapper
 import android.text.style.ClickableSpan
 import android.view.View
 import me.proxer.app.base.BaseActivity
@@ -8,6 +10,12 @@ import okhttp3.HttpUrl
 internal class UrlClickableSpan(private val url: HttpUrl) : ClickableSpan() {
 
     override fun onClick(widget: View) {
-        (widget.context as? BaseActivity)?.showPage(url)
+        findBaseActivity(widget.context)?.showPage(url)
+    }
+
+    private fun findBaseActivity(currentContext: Context): BaseActivity? = when (currentContext) {
+        is BaseActivity -> currentContext
+        is ContextWrapper -> findBaseActivity(currentContext.baseContext)
+        else -> null
     }
 }
