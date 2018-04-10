4d25ff0d84e7455e8a5a9429e7c64cf2150a5d3f
Ruben Gees <rubengees7@googlemail.com>
2018-04-10 15:41:02 +0200

    Fix memory leak in the schedule screen

--- src/main/kotlin/me/proxer/app/anime/ScheduleEntryAdapter.kt
+++ src/main/kotlin/me/proxer/app/anime/ScheduleEntryAdapter.kt
@@ -109,6 +109,11 @@ class ScheduleEntryAdapter : BaseAdapter<CalendarEntry, ViewHolder>() {
         cachedViewHolders -= holder
     }
 
+    override fun onDetachedFromRecyclerView(recyclerView: RecyclerView) {
+        layoutManager = null
+        glide = null
+    }
+
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         internal val image by bindView<ImageView>(R.id.image)
