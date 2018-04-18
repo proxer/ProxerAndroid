4a71f92e1e8b9699db9da3b5483e0c8a862a158c
Ruben Gees <rubengees7@googlemail.com>
2018-04-18 12:13:10 +0200

    Add attribution for RetryWithDelay and fix code style

--- src/main/kotlin/me/proxer/app/util/RxRetryWithDelay.kt
+++ src/main/kotlin/me/proxer/app/util/RxRetryWithDelay.kt
@@ -4,18 +4,21 @@ import io.reactivex.Flowable
 import io.reactivex.functions.Function
 import java.util.concurrent.TimeUnit
 
+/**
+ * Stolen and adjusted from here: https://stackoverflow.com/a/25292833/4279995
+ */
 class RxRetryWithDelay(
-        private val maxRetries: Int,
-        private val retryDelayMillis: Long
+    private val maxRetries: Int,
+    private val retryDelayMillis: Long
 ) : Function<Flowable<out Throwable>, Flowable<*>> {
 
     private var retryCount: Int = 0
 
     override fun apply(attempts: Flowable<out Throwable>): Flowable<Any> = attempts
-            .flatMap { throwable ->
-                when {
-                    ++retryCount < maxRetries -> Flowable.timer(retryDelayMillis, TimeUnit.MILLISECONDS)
-                    else -> Flowable.error(throwable)
-                }
+        .flatMap { throwable ->
+            when {
+                ++retryCount < maxRetries -> Flowable.timer(retryDelayMillis, TimeUnit.MILLISECONDS)
+                else -> Flowable.error(throwable)
             }
+        }
 }
