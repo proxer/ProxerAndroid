73722d5c8a88d039faa612d69d31b724da50f1d1
Ruben Gees <rubengees7@googlemail.com>
2018-05-14 21:30:40 +0200

    Add support for named quotes

--- src/main/kotlin/me/proxer/app/ui/view/bbcode/prototype/QuotePrototype.kt
+++ src/main/kotlin/me/proxer/app/ui/view/bbcode/prototype/QuotePrototype.kt
@@ -8,44 +8,55 @@ import android.widget.LinearLayout
 import android.widget.LinearLayout.LayoutParams
 import android.widget.LinearLayout.LayoutParams.MATCH_PARENT
 import android.widget.LinearLayout.LayoutParams.WRAP_CONTENT
-import android.widget.LinearLayout.VERTICAL
+import me.proxer.app.MainApplication.Companion.globalContext
 import me.proxer.app.R
 import me.proxer.app.ui.view.bbcode.BBArgs
 import me.proxer.app.ui.view.bbcode.BBTree
+import me.proxer.app.ui.view.bbcode.BBUtils
 import me.proxer.app.ui.view.bbcode.prototype.BBPrototype.Companion.REGEX_OPTIONS
 import org.jetbrains.anko.dip
 
 object QuotePrototype : AutoClosingPrototype {
 
+    private val QUOTE_ATTRIBUTE_REGEX = Regex("quote *= *(.+?)( |$)", REGEX_OPTIONS)
+
     override val startRegex = Regex(" *quote( *=\"?.+?\"?)?( .*?)?", REGEX_OPTIONS)
     override val endRegex = Regex("/ *quote *", REGEX_OPTIONS)
+
+    override fun construct(code: String, parent: BBTree): BBTree {
+        val quote = BBUtils.cutAttribute(code, QUOTE_ATTRIBUTE_REGEX)
+
+        if (quote != null) {
+            val quoteText = globalContext.getString(R.string.view_bbcode_quote, quote.trim())
+            val quoteTree = BoldPrototype.construct("", parent)
+
+            quoteTree.children.add(TextPrototype.construct(quoteText, quoteTree))
+            parent.children.add(quoteTree)
+        }
+
+        return BBTree(this, parent)
+    }
 
     override fun makeViews(context: Context, children: List<BBTree>, args: BBArgs): List<View> {
         val childViews = super.makeViews(context, children, args)
 
-        return when (childViews.size) {
-            0 -> childViews
-            1 -> listOf(FrameLayout(context).apply {
-                val fourDip = dip(4)
-
-                layoutParams = LayoutParams(MATCH_PARENT, WRAP_CONTENT)
-
-                setPadding(fourDip, fourDip, fourDip, fourDip)
-                setBackgroundColor(ContextCompat.getColor(context, R.color.selected))
-
-                childViews.forEach { addView(it) }
-            })
-            else -> listOf(LinearLayout(context).apply {
-                val fourDip = dip(4)
-
-                layoutParams = LayoutParams(MATCH_PARENT, WRAP_CONTENT)
-                orientation = VERTICAL
-
-                setPadding(fourDip, fourDip, fourDip, fourDip)
-                setBackgroundColor(ContextCompat.getColor(context, R.color.selected))
-
-                childViews.forEach { addView(it) }
-            })
+        val layout = when (childViews.size) {
+            0 -> null
+            1 -> FrameLayout(context)
+            else -> LinearLayout(context)
         }
+
+        layout?.apply {
+            val fourDip = context.dip(4)
+
+            layoutParams = LayoutParams(MATCH_PARENT, WRAP_CONTENT)
+
+            setPadding(fourDip, fourDip, fourDip, fourDip)
+            setBackgroundColor(ContextCompat.getColor(context, R.color.selected))
+
+            childViews.forEach { addView(it) }
+        }
+
+        return if (layout != null) listOf(layout) else emptyList()
     }
 }
