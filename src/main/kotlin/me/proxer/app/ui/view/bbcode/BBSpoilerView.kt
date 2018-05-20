fa3c9fde8fc6731f808b2772ce5ddb086992a3d8
Ruben Gees <rubengees7@googlemail.com>
2018-05-20 22:25:26 +0200

    Fix layout issues of BBCode spoilers in dynamic contexts

--- src/main/kotlin/me/proxer/app/ui/view/bbcode/BBSpoilerView.kt
+++ src/main/kotlin/me/proxer/app/ui/view/bbcode/BBSpoilerView.kt
@@ -1,27 +1,20 @@
 package me.proxer.app.ui.view.bbcode
 
 import android.content.Context
-import android.graphics.Typeface.DEFAULT_BOLD
-import android.os.Build
-import android.support.v4.content.ContextCompat
 import android.support.v4.view.ViewCompat
-import android.support.v4.widget.TextViewCompat
-import android.support.v7.widget.AppCompatTextView
 import android.util.AttributeSet
-import android.util.TypedValue
-import android.view.Gravity.CENTER
-import android.view.Gravity.CENTER_HORIZONTAL
-import android.view.Gravity.CENTER_VERTICAL
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TextView
 import com.mikepenz.community_material_typeface_library.CommunityMaterial
 import com.mikepenz.iconics.IconicsDrawable
+import kotterknife.bindView
 import me.proxer.app.R
-import org.jetbrains.anko.dip
 
 /**
  * @author Ruben Gees
@@ -44,7 +37,7 @@ internal class BBSpoilerView @JvmOverloads constructor(
         set(value) {
             toggleText.setTextColor(value)
 
-            initToggleButton()
+            updateToggleButtonIcon()
         }
 
     private var isExpanded = false
@@ -62,40 +55,25 @@ internal class BBSpoilerView @JvmOverloads constructor(
             }
         }
 
-    private val toggle = LinearLayout(context)
-    private val toggleText = AppCompatTextView(context)
-    private val toggleButton = ImageView(context)
+    private val toggle by bindView<LinearLayout>(R.id.toggle)
+    private val toggleText by bindView<TextView>(R.id.toggleText)
+    private val toggleButton by bindView<ImageView>(R.id.toggleButton)
 
-    private val decoration = LinearLayout(context)
-    private val space = View(context)
-    private val container = LinearLayout(context)
-
-    private val fourDip = dip(4)
-    private val twoDip = dip(2)
+    private val decoration by bindView<LinearLayout>(R.id.decoration)
+    private val container by bindView<LinearLayout>(R.id.container)
 
     init {
-        orientation = VERTICAL
         layoutParams = ViewGroup.MarginLayoutParams(MATCH_PARENT, WRAP_CONTENT)
+        orientation = VERTICAL
 
-        toggle.orientation = HORIZONTAL
-        container.orientation = VERTICAL
-        decoration.orientation = HORIZONTAL
+        LayoutInflater.from(context).inflate(R.layout.view_bb_spoiler, this, true)
 
-        space.setBackgroundColor(ContextCompat.getColor(context, R.color.divider))
+        toggle.setOnClickListener { isExpanded = !isExpanded }
 
-        initToggle()
-        initToggleText()
-        initDecoration()
-        initToggleButton()
+        toggleText.setTextColor(spoilerTextColor)
+        toggleText.setTag(R.id.ignore_tag, Unit)
 
-        addView(toggle, LayoutParams(WRAP_CONTENT, WRAP_CONTENT).apply {
-            gravity = CENTER_HORIZONTAL
-        })
-
-        addView(decoration, LayoutParams(MATCH_PARENT, WRAP_CONTENT).apply {
-            topMargin = fourDip
-        })
-
+        updateToggleButtonIcon()
         handleExpansion()
     }
 
@@ -103,46 +81,11 @@ internal class BBSpoilerView @JvmOverloads constructor(
         container.addView(child)
     }
 
-    private fun initToggle() {
-        toggle.addView(toggleText, LayoutParams(WRAP_CONTENT, WRAP_CONTENT, 1f).apply {
-            gravity = CENTER_VERTICAL
-        })
-
-        toggle.addView(toggleButton, LayoutParams(dip(32), dip(32)).apply {
-            gravity = CENTER_VERTICAL
-        })
+    override fun removeAllViews() {
+        container.removeAllViews()
     }
 
-    private fun initToggleText() {
-        val selectableItemBackground = TypedValue().apply {
-            context.theme.resolveAttribute(R.attr.selectableItemBackground, this, true)
-        }
-
-        TextViewCompat.setTextAppearance(toggleText, R.style.TextAppearance_AppCompat_Medium)
-
-        toggle.setOnClickListener { isExpanded = !isExpanded }
-        toggle.setBackgroundResource(selectableItemBackground.resourceId)
-
-        toggleText.setPadding(fourDip, twoDip, fourDip, twoDip)
-        toggleText.setTextColor(spoilerTextColor)
-        toggleText.setTag(R.id.ignore_tag, Unit)
-        toggleText.typeface = DEFAULT_BOLD
-        toggleText.gravity = CENTER
-    }
-
-    private fun initDecoration() {
-        decoration.addView(space, LayoutParams(twoDip, MATCH_PARENT).apply {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-                marginEnd = fourDip
-            } else {
-                rightMargin = fourDip
-            }
-        })
-
-        decoration.addView(container, LayoutParams(MATCH_PARENT, WRAP_CONTENT))
-    }
-
-    private fun initToggleButton() {
+    private fun updateToggleButtonIcon() {
         toggleButton.setImageDrawable(IconicsDrawable(context, CommunityMaterial.Icon.cmd_chevron_down)
             .sizeDp(32)
             .paddingDp(8)
@@ -151,6 +94,7 @@ internal class BBSpoilerView @JvmOverloads constructor(
 
     private fun handleExpansion() {
         decoration.visibility = if (isExpanded) View.VISIBLE else View.GONE
+
         toggleText.text = when {
             spoilerTitle != null -> spoilerTitle
             isExpanded -> context.getString(R.string.view_bbcode_hide_spoiler)
@@ -161,6 +105,8 @@ internal class BBSpoilerView @JvmOverloads constructor(
             true -> ViewCompat.animate(toggleButton).rotation(180f)
             false -> ViewCompat.animate(toggleButton).rotation(0f)
         }
+
+        toggleText.requestLayout()
     }
 
     private fun findHost(): BBCodeView? {
