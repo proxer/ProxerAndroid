23ad2fca39bbc36798efdb9ccdfe5820fcbe5813
Ruben Gees <rubengees7@googlemail.com>
2018-06-16 19:23:32 +0200

    Add support for the viewster app

--- src/main/kotlin/me/proxer/app/anime/resolver/ViewsterStreamResolver.kt
+++ src/main/kotlin/me/proxer/app/anime/resolver/ViewsterStreamResolver.kt
@@ -1,7 +1,10 @@
 package me.proxer.app.anime.resolver
 
+import android.content.Intent
 import io.reactivex.Single
 import me.proxer.app.MainApplication.Companion.api
+import me.proxer.app.MainApplication.Companion.globalContext
+import me.proxer.app.exception.AppRequiredException
 import me.proxer.app.util.Utils
 import me.proxer.app.util.extension.androidUri
 import me.proxer.app.util.extension.buildSingle
@@ -11,9 +14,22 @@ import me.proxer.app.util.extension.buildSingle
  */
 class ViewsterStreamResolver : StreamResolver {
 
+    companion object {
+        private const val VIEWSTER_PACKAGE = "com.viewster.androidapp"
+    }
+
     override val name = "Viewster"
 
-    override fun resolve(id: String): Single<StreamResolutionResult> = api.anime().link(id)
-        .buildSingle()
-        .map { StreamResolutionResult(Utils.parseAndFixUrl(it).androidUri(), "text/html") }
+    override fun resolve(id: String): Single<StreamResolutionResult> = Single
+        .fromCallable {
+            if (!Utils.isPackageInstalled(globalContext.packageManager, VIEWSTER_PACKAGE)) {
+                throw AppRequiredException(name, VIEWSTER_PACKAGE)
+            }
+        }
+        .flatMap { api.anime().link(id).buildSingle() }
+        .map {
+            val uri = Utils.parseAndFixUrl(it.replace("/embed", "")).androidUri()
+
+            StreamResolutionResult(Intent(Intent.ACTION_VIEW, uri).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+        }
 }
