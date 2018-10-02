649c168f850dc430fa98aa7155ecc74b995d2e86
Ruben Gees <rubengees7@googlemail.com>
2018-10-02 22:26:02 +0200

    Explicitly set field names of LocalUser

--- src/main/kotlin/me/proxer/app/auth/LocalUser.kt
+++ src/main/kotlin/me/proxer/app/auth/LocalUser.kt
@@ -1,9 +1,15 @@
 package me.proxer.app.auth
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 /**
  * @author Ruben Gees
  */
 @JsonClass(generateAdapter = true)
-data class LocalUser(val token: String, val id: String, val name: String, val image: String)
+data class LocalUser(
+    @Json(name = "token") val token: String,
+    @Json(name = "id") val id: String,
+    @Json(name = "name") val name: String,
+    @Json(name = "image") val image: String
+)
