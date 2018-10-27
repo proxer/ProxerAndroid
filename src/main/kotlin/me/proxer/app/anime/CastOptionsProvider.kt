6052d7200532b953cec4b1123673eef342f4546d
Ruben Gees <rubengees7@googlemail.com>
2018-10-27 14:14:10 +0200

    Add custom cast options

---
+++ src/main/kotlin/me/proxer/app/anime/CastOptionsProvider.kt
@@ -0,0 +1,35 @@
+package me.proxer.app.anime
+
+import android.content.Context
+import com.google.android.gms.cast.CastMediaControlIntent
+import com.google.android.gms.cast.framework.CastOptions
+import com.google.android.gms.cast.framework.OptionsProvider
+import com.google.android.gms.cast.framework.SessionProvider
+import com.google.android.gms.cast.framework.media.CastMediaOptions
+import com.google.android.gms.cast.framework.media.NotificationOptions
+import com.google.android.gms.common.annotation.KeepName
+
+@KeepName
+@Suppress("unused")
+class CastOptionsProvider : OptionsProvider {
+
+    override fun getCastOptions(context: Context): CastOptions {
+        val notificationOptions = NotificationOptions.Builder()
+            .setTargetActivityClassName(StreamActivity::class.java.name)
+            .build()
+
+        val mediaOptions = CastMediaOptions.Builder()
+            .setNotificationOptions(notificationOptions)
+            .build()
+
+        return CastOptions.Builder()
+            .setReceiverApplicationId(CastMediaControlIntent.DEFAULT_MEDIA_RECEIVER_APPLICATION_ID)
+            .setStopReceiverApplicationWhenEndingSession(true)
+            .setCastMediaOptions(mediaOptions)
+            .build()
+    }
+
+    override fun getAdditionalSessionProviders(context: Context): List<SessionProvider>? {
+        return null
+    }
+}
