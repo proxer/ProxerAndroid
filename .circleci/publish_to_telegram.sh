dc9f0924ebc0dc3580e2bed4d045d9c8f20cea87
Ruben Gees <rubengees7@googlemail.com>
2018-12-16 23:51:25 +0100

    Use commit range for telegram caption on CI

--- .circleci/publish_to_telegram.sh
+++ .circleci/publish_to_telegram.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+if [[ -n "$CIRCLE_COMPARE_URL" ]]; then
+    COMMIT_RANGE=$(echo "${CIRCLE_COMPARE_URL}" | cut -d/ -f7)
+    CAPTION=$(git log --pretty=format:'- %s' "${COMMIT_RANGE}")
+else
+    CAPTION=$(git log -1 --pretty=format:'- %s')
+fi
+
+CAPTION=$(echo "$CAPTION" | cut -c -1024)
+
 curl -s -S \
 -F chat_id="$TELEGRAM_CHAT_ID" \
--F caption="$(git log -1 --pretty=format:'%s')" \
+-F caption="$CAPTION" \
 -F document=@$(find ./build/outputs/apk/logRelease/ -type f -name "*.apk") \
 https://api.telegram.org/bot${TELEGRAM_BOT_ID}/sendDocument > /dev/null
