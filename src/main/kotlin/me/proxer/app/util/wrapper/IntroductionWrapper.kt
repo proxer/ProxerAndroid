cc0de0f6f3fb8bd24baf85ad62acfde191399fdf
Ruben Gees <rubengees7@googlemail.com>
2018-12-18 12:22:28 +0100

    Fix introduction screen crashing due to new color system

--- src/main/kotlin/me/proxer/app/util/wrapper/IntroductionWrapper.kt
+++ src/main/kotlin/me/proxer/app/util/wrapper/IntroductionWrapper.kt
@@ -7,14 +7,13 @@ import android.widget.TextView
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.view.setPadding
 import com.mikepenz.community_material_typeface_library.CommunityMaterial
+import com.mikepenz.iconics.IconicsDrawable
 import com.rubengees.introduction.IntroductionBuilder
 import com.rubengees.introduction.Option
 import com.rubengees.introduction.Slide
 import com.rubengees.introduction.interfaces.OnSlideListener
 import me.proxer.app.R
 import me.proxer.app.util.DeviceUtils
-import me.proxer.app.util.extension.resolveColor
-import me.proxer.app.util.extension.setIconicsImage
 
 /**
  * @author Ruben Gees
@@ -33,8 +32,11 @@ object IntroductionWrapper {
                         image.setPadding(padding)
                         image.setImageDrawable(AppCompatResources.getDrawable(activity, R.drawable.ic_proxer))
                     }
-                    1 -> image.setIconicsImage(
-                        CommunityMaterial.Icon.cmd_bell_outline, 256, padding / 2, R.attr.colorOnPrimary
+                    1 -> image.setImageDrawable(
+                        IconicsDrawable(image.context, CommunityMaterial.Icon.cmd_bell_outline)
+                            .sizeDp(256)
+                            .paddingDp(padding / 2)
+                            .colorRes(R.color.on_primary)
                     )
                 }
             }
@@ -43,10 +45,10 @@ object IntroductionWrapper {
 
     private fun generateSlides(context: Context) = arrayListOf(
         Slide().withTitle(R.string.introduction_welcome_title)
-            .withColor(context.resolveColor(R.attr.colorPrimary))
+            .withColorResource(R.color.primary)
             .withDescription(R.string.introduction_welcome_description),
         Slide().withTitle(R.string.introduction_notifications_title)
-            .withColor(context.resolveColor(R.attr.colorPrimary))
+            .withColorResource(R.color.primary)
             .withOption(Option(context.getString(R.string.introduction_notifications_description), true))
     )
 }
