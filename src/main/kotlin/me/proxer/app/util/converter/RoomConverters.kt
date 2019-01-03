028149f1ede26d5dccc465429c4de10d398b4b87
Ruben Gees <rubengees7@googlemail.com>
2019-01-03 02:24:30 +0100

    Remove some unused code

--- src/main/kotlin/me/proxer/app/util/converter/RoomConverters.kt
+++ src/main/kotlin/me/proxer/app/util/converter/RoomConverters.kt
@@ -14,10 +14,6 @@ import java.util.Date
 @Suppress("unused")
 class RoomConverters {
 
-    private companion object {
-        private const val DELIMITER = ";"
-    }
-
     @TypeConverter
     fun fromTimestamp(value: Long?) = value?.let { Date(it) }
 
