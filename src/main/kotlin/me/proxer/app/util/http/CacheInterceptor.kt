415014427358cc235db0f7373df912cc9c7d9712
Ruben Gees <rubengees7@googlemail.com>
2019-01-04 01:37:18 +0100

    Refine http caching to only cache relevant things

--- src/main/kotlin/me/proxer/app/util/http/CacheInterceptor.kt
+++ src/main/kotlin/me/proxer/app/util/http/CacheInterceptor.kt
@@ -5,6 +5,8 @@ import okhttp3.CacheControl
 import okhttp3.HttpUrl
 import okhttp3.Interceptor
 import okhttp3.Response
+import okhttp3.ResponseBody
+import okio.BufferedSource
 import okio.GzipSource
 import okio.buffer
 import org.threeten.bp.LocalDate
@@ -12,6 +14,7 @@ import org.threeten.bp.LocalDateTime
 import org.threeten.bp.ZoneId
 import org.threeten.bp.temporal.ChronoUnit
 import java.util.concurrent.TimeUnit
+import kotlin.contracts.ExperimentalContracts
 
 /**
  * @author Ruben Gees
@@ -19,18 +22,21 @@ import java.util.concurrent.TimeUnit
 class CacheInterceptor : Interceptor {
 
     private companion object {
-        private val successRegex = Regex(".*\"error\": *?0.*")
+        private val apiSuccessRegex = Regex(".*\"error\": *?0.*", setOf(RegexOption.DOT_MATCHES_ALL))
+        private val streamSuccessRegex = Regex(".*<source.*?>.*", setOf(RegexOption.DOT_MATCHES_ALL))
+
         private val zoneIdBerlin by unsafeLazy { ZoneId.of("Europe/Berlin") }
 
-        private val cacheInfoList = listOf(
-            CacheInfo(HttpUrl.get("https://proxer.me/api/v1/info/fullentry"), 24),
-            CacheInfo(HttpUrl.get("https://proxer.me/api/v1/info/entry"), 24),
-            CacheInfo(HttpUrl.get("https://proxer.me/api/v1/list/entrysearch"), 1),
-            CacheInfo(HttpUrl.get("https://proxer.me/api/v1/manga/chapter"), 24),
-            CacheInfo(HttpUrl.get("https://proxer.me/api/v1/anime/proxerstreams"), 1),
-            CacheInfo(HttpUrl.get("https://proxer.me/api/v1/anime/link"), 1),
+        private val cacheInfo = listOf(
+            CacheInfo("https://proxer.me/api/v1/info/fullentry", 24),
+            CacheInfo("https://proxer.me/api/v1/info/entry", 24),
+            CacheInfo("https://proxer.me/api/v1/list/entrysearch", 1),
+            CacheInfo("https://proxer.me/api/v1/manga/chapter", 24),
+            CacheInfo("https://proxer.me/api/v1/anime/proxerstreams", 1),
+            CacheInfo("https://proxer.me/api/v1/anime/link", 1),
+            CacheInfo("https://stream.proxer.me", 24),
             CacheInfo(
-                HttpUrl.get("https://proxer.me/api/v1/media/calendar"),
+                "https://proxer.me/api/v1/media/calendar",
                 {
                     val now = LocalDateTime.now(zoneIdBerlin)
                     val tomorrow = LocalDate.now(zoneIdBerlin).plusDays(1).atTime(0, 0)
@@ -40,44 +46,95 @@ class CacheInterceptor : Interceptor {
                 TimeUnit.SECONDS
             )
         )
+
+        private val excludedFileTypes = listOf(".png", ".jpg", ".jpeg", ".gif", ".webm")
     }
 
+    @ExperimentalContracts
     override fun intercept(chain: Interceptor.Chain): Response {
         val response = chain.proceed(chain.request())
+        val applicableCacheInfo = cacheInfo.find { (url) -> response.request().url().toString().startsWith(url) }
 
-        return if (
-            response.isSuccessful &&
-            response.request().method().equals("GET", true)
-        ) {
-            val currentUrl = response.request().url().toString()
-            val applicableCacheInfo = cacheInfoList.find { (url) -> currentUrl.startsWith(url.toString()) }
-
-            if (applicableCacheInfo != null) {
-                val body = response.body()?.source()?.peek()?.let { GzipSource(it) }?.buffer()?.use {
-                    it.readUtf8(16)
-                }
-
-                if (body == null || body.matches(successRegex)) {
-                    val cacheControl = CacheControl.Builder()
-                        .maxAge(applicableCacheInfo.maxAge(), applicableCacheInfo.timeUnit)
-                        .build()
-
-                    response.newBuilder()
-                        .header("Cache-Control", cacheControl.toString())
-                        .removeHeader("Pragma")
-                        .build()
-                } else {
-                    return response
-                }
-            } else {
-                response
+        return when {
+            applicableCacheInfo != null && shouldEnableCacheForResponse(response) -> response.setCacheControl {
+                maxAge(applicableCacheInfo.maxAge(), applicableCacheInfo.timeUnit)
             }
-        } else {
-            response
+            shouldDisableCacheForResponse(response) -> response.setCacheControl {
+                noCache()
+                noStore()
+            }
+            else -> response
         }
     }
 
-    private data class CacheInfo(val url: HttpUrl, val maxAge: () -> Int, val timeUnit: TimeUnit = TimeUnit.HOURS) {
-        constructor(url: HttpUrl, maxAge: Int, timeUnit: TimeUnit = TimeUnit.HOURS) : this(url, { maxAge }, timeUnit)
+    private fun shouldEnableCacheForResponse(response: Response) = response.isSuccessful &&
+        response.request().method().equals("GET", true) &&
+        isSuccessfulBody(response)
+
+    private fun shouldDisableCacheForResponse(response: Response) = response.header("Cache-Control") == null ||
+        excludedFileTypes.any { response.request().url().toString().endsWith(it) }
+
+    private fun isSuccessfulBody(response: Response): Boolean {
+        val url = response.request().url().toString()
+
+        return when {
+            url.contains("proxer.me/api/v1") -> {
+                response.body()
+                    ?.peekAndUseWithGzip {
+                        it.readUtf8(12).matches(apiSuccessRegex)
+                    }
+                    ?: false
+            }
+            url.contains("stream.proxer.me") -> {
+                response.body()
+                    ?.peekAndUseWithGzip {
+                        var found: Boolean? = null
+
+                        while (found == null) {
+                            val nextLine = it.readUtf8Line()
+
+                            if (nextLine == null) {
+                                found = false
+                            } else if (nextLine.matches(streamSuccessRegex)) {
+                                found = true
+                            }
+                        }
+
+                        found
+                    }
+                    ?: false
+            }
+            else -> false
+        }
+    }
+
+    private inline fun Response.setCacheControl(action: CacheControl.Builder.() -> Unit): Response {
+        val cacheControl = CacheControl.Builder()
+            .apply { action(this) }
+            .build()
+
+        return newBuilder()
+            .header("Cache-Control", cacheControl.toString())
+            .removeHeader("Pragma")
+            .build()
+    }
+
+    private inline fun <R> ResponseBody.peekAndUseWithGzip(block: (BufferedSource) -> R) = this
+        .source()
+        .peek()
+        .let { GzipSource(it) }
+        .buffer()
+        .use(block)
+
+    private class CacheInfo(
+        url: String,
+        val maxAge: () -> Int,
+        val timeUnit: TimeUnit = TimeUnit.HOURS
+    ) {
+        val url = HttpUrl.get(url).toString()
+
+        constructor(url: String, maxAge: Int, timeUnit: TimeUnit = TimeUnit.HOURS) : this(url, { maxAge }, timeUnit)
+
+        operator fun component1() = url
     }
 }
