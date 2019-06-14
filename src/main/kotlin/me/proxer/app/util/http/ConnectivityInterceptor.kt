38f52b43af619562a8448dc040a2e60754d7906c
Ruben Gees <rubengees7@googlemail.com>
2019-06-15 01:44:56 +0200

    Safeguard against crash on devices without wireless settings

--- src/main/kotlin/me/proxer/app/util/http/ConnectivityInterceptor.kt
+++ src/main/kotlin/me/proxer/app/util/http/ConnectivityInterceptor.kt
@@ -1,7 +1,10 @@
 package me.proxer.app.util.http
 
 import android.content.Context
+import android.content.Intent
 import android.net.ConnectivityManager
+import android.os.Build
+import android.provider.Settings
 import androidx.core.content.getSystemService
 import me.proxer.app.exception.NotConnectedException
 import me.proxer.app.util.compat.isConnected
@@ -16,8 +19,11 @@ class ConnectivityInterceptor(context: Context) : Interceptor, KoinComponent {
 
     private val connectivityManager = requireNotNull(context.getSystemService<ConnectivityManager>())
 
+    private val hasConnectionSettings = Build.VERSION.SDK_INT < Build.VERSION_CODES.Q &&
+        Intent(Settings.ACTION_WIRELESS_SETTINGS).resolveActivity(context.packageManager) != null
+
     override fun intercept(chain: Interceptor.Chain): Response {
-        if (!connectivityManager.isConnected) {
+        if (hasConnectionSettings && !connectivityManager.isConnected) {
             throw NotConnectedException()
         }
 
