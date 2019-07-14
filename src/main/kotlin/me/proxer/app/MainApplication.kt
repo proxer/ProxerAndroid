95382f88f339c408b4892a1429d65f405f24b380
Ruben Gees <rubengees7@googlemail.com>
2019-07-14 20:51:10 +0200

    Move initialization code back into MainApplication

--- src/main/kotlin/me/proxer/app/MainApplication.kt
+++ src/main/kotlin/me/proxer/app/MainApplication.kt
@@ -2,21 +2,41 @@ package me.proxer.app
 
 import android.annotation.SuppressLint
 import android.app.Application
+import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Environment
+import android.os.Looper
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.work.Configuration
+import androidx.work.Logger
+import androidx.work.WorkManager
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.common.GooglePlayServicesUtil
+import com.google.android.gms.security.ProviderInstaller
+import com.jakewharton.threetenabp.AndroidThreeTen
+import com.kirillr.strictmodehelper.StrictModeCompat
 import com.mikepenz.iconics.Iconics
 import com.mikepenz.materialdrawer.util.DrawerImageLoader
 import com.vanniktech.emoji.EmojiManager
 import com.vanniktech.emoji.ios.IosEmojiProvider
 import io.reactivex.Completable
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.exceptions.UndeliverableException
+import io.reactivex.plugins.RxJavaPlugins
 import io.reactivex.schedulers.Schedulers
 import me.proxer.app.auth.LoginHandler
 import me.proxer.app.util.GlideDrawerImageLoader
 import me.proxer.app.util.NotificationUtils
 import me.proxer.app.util.data.PreferenceHelper
+import me.proxer.app.util.extension.isPackageInstalled
+import me.proxer.app.util.logging.TimberFileTree
+import me.proxer.app.util.logging.WorkManagerTimberLogger
 import org.koin.android.ext.android.inject
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import timber.log.Timber
 
 /**
  * @author Ruben Gees
@@ -34,6 +54,16 @@ class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        enableStrictModeForDebug()
+        initGlobalErrorHandler()
+        initSecurity()
+
+        startKoin {
+            androidContext(this@MainApplication)
+
+            modules(koinModules)
+        }
+
         FlavorInitializer.initialize(this)
         NotificationUtils.createNotificationChannels(this)
 
@@ -44,7 +74,83 @@ class MainApplication : Application() {
         loginHandler.listen(this)
     }
 
+    private fun initGlobalErrorHandler() {
+        val oldHandler = Thread.getDefaultUncaughtExceptionHandler()
+
+        Thread.setDefaultUncaughtExceptionHandler { thread, error ->
+            Timber.e(error)
+
+            oldHandler?.uncaughtException(thread, error)
+        }
+    }
+
+    private fun initSecurity() {
+        ProviderInstaller.installIfNeededAsync(this, object : ProviderInstaller.ProviderInstallListener {
+            override fun onProviderInstallFailed(errorCode: Int, recoveryIntent: Intent?) {
+                GoogleApiAvailability.getInstance().apply {
+                    Timber.e("Error installing security patches with error code $errorCode")
+
+                    if (
+                        isUserResolvableError(errorCode) &&
+                        packageManager.isPackageInstalled(GooglePlayServicesUtil.GOOGLE_PLAY_STORE_PACKAGE)
+                    ) {
+                        showErrorNotification(this@MainApplication, errorCode)
+                    }
+                }
+            }
+
+            override fun onProviderInstalled() = Unit
+        })
+    }
+
+    private fun enableStrictModeForDebug() {
+        if (BuildConfig.DEBUG) {
+            val threadPolicy = StrictModeCompat.ThreadPolicy.Builder()
+                .detectAll()
+                .permitCustomSlowCalls()
+                .permitDiskWrites()
+                .permitDiskReads()
+                .penaltyDialog()
+                .penaltyLog()
+                .build()
+
+            val vmPolicy = StrictModeCompat.VmPolicy.Builder()
+                .detectAll()
+                .penaltyLog()
+                .build()
+
+            StrictModeCompat.setPolicies(threadPolicy, vmPolicy)
+        }
+    }
+
+    // TODO: Remove once api becomes public.
+    @SuppressLint("RestrictedApi")
     private fun initLibs() {
+        WorkManager.initialize(this, Configuration.Builder().build())
+        AndroidThreeTen.init(this)
+
+        if (BuildConfig.LOG) {
+            Timber.plant(TimberFileTree(this))
+
+            if (BuildConfig.DEBUG) {
+                Timber.plant(Timber.DebugTree())
+            }
+        }
+
+        Logger.setLogger(WorkManagerTimberLogger())
+
+        RxJavaPlugins.setErrorHandler { error ->
+            when (error) {
+                is UndeliverableException -> Timber.e(error, "Can't deliver error")
+                is InterruptedException -> Timber.w(error)
+                else -> Thread.currentThread().uncaughtExceptionHandler
+                    ?.uncaughtException(Thread.currentThread(), error)
+            }
+        }
+
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler { AndroidSchedulers.from(Looper.getMainLooper(), true) }
+        RxAndroidPlugins.setMainThreadSchedulerHandler { AndroidSchedulers.from(Looper.getMainLooper(), true) }
+
         Iconics.init(this)
 
         SubsamplingScaleImageView.setPreferredBitmapConfig(Bitmap.Config.RGB_565)
