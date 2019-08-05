582b4677c6e79d82cd0c09d08b22925a18517beb
Ruben Gees <rubengees7@googlemail.com>
2019-08-05 22:39:19 +0200

    Make circleci bash scripts not crash on missing variables

--- .circleci/create_secrets.sh
+++ .circleci/create_secrets.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [[ -n "$KEYSTORE" ]] && [[ -n "$SECRETS" ]]; then
-	echo ${KEYSTORE} | base64 -d >keystore.jks
-	echo ${SECRETS} | base64 -d >secrets.properties
+if [[ -v KEYSTORE ]] && [[ -v SECRETS ]]; then
+	echo "${KEYSTORE}" | base64 -d >keystore.jks
+	echo "${SECRETS}" | base64 -d >secrets.properties
 	echo "Created secrets"
 else
-	echo "Environment variables not set; Skipped"
+	echo "Environment variables not set, skipping."
 fi
--- .circleci/publish_to_telegram.sh
+++ .circleci/publish_to_telegram.sh
@@ -1,21 +1,26 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [[ -n "${CIRCLE_COMPARE_URL:-}" ]]; then
+if [[ -v CIRCLE_COMPARE_URL ]]; then
 	COMMIT_RANGE=$(echo "${CIRCLE_COMPARE_URL}" | cut -d/ -f7)
 	CAPTION=$(git log --pretty=format:"- %s" "${COMMIT_RANGE}" --reverse) || true
 else
 	CAPTION=$(git log -1 --pretty=format:"- %s") || true
 fi
 
-if [[ -n "${CAPTION:-}" ]]; then
+if [[ -v CAPTION ]]; then
     CAPTION=$(echo "$CAPTION" | cut -c -1024)
 else
 	CAPTION="No changelog available"
 fi
 
-curl -s -S \
-	-F chat_id="$TELEGRAM_CHAT_ID" \
-	-F caption="$CAPTION" \
-	-F document=@$(find ./build/outputs/apk/logRelease/ -type f -name "*.apk") \
-	https://api.telegram.org/bot${TELEGRAM_BOT_ID}/sendDocument >/dev/null
+if [[ -v TELEGRAM_CHAT_ID ]] && [[ -v TELEGRAM_BOT_ID ]]; then
+    curl -s -S \
+        -F chat_id="$TELEGRAM_CHAT_ID" \
+        -F caption="$CAPTION" \
+        -F document=@$(find ./build/outputs/apk/logRelease/ -type f -name "*.apk") \
+        https://api.telegram.org/bot${TELEGRAM_BOT_ID}/sendDocument >/dev/null
+else
+    echo "TELEGRAM_CHAT_ID or TELEGRAM_BOT_ID not set."
+    exit -1
+fi
