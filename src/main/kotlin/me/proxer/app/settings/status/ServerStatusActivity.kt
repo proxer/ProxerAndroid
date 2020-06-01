16761d80f744bc0d70eb7ea937b37fde2ca9dde4
Ruben Gees <rubengees7@googlemail.com>
2020-06-01 14:59:16 +0200

    Fix empty drawer menu in server status screen
    Signed-off-by: Ruben Gees <rubengees7@googlemail.com>

--- src/main/kotlin/me/proxer/app/settings/status/ServerStatusActivity.kt
+++ src/main/kotlin/me/proxer/app/settings/status/ServerStatusActivity.kt
@@ -2,29 +2,22 @@ package me.proxer.app.settings.status
 
 import android.app.Activity
 import android.os.Bundle
-import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.commitNow
-import kotterknife.bindView
 import me.proxer.app.R
-import me.proxer.app.base.BaseActivity
+import me.proxer.app.base.DrawerActivity
 import me.proxer.app.util.extension.startActivity
 
 /**
  * @author Ruben Gees
  */
-class ServerStatusActivity : BaseActivity() {
+class ServerStatusActivity : DrawerActivity() {
 
     companion object {
         fun navigateTo(context: Activity) = context.startActivity<ServerStatusActivity>()
     }
 
-    private val toolbar: Toolbar by bindView(R.id.toolbar)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.activity_default)
-        setSupportActionBar(toolbar)
 
         title = getString(R.string.section_server_status)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
